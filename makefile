17d0ff00f8d51076fa602184687c6ae5ae1ba9d9
Aaron Li <aaronli39@gmail.com>
2018-10-18 22:33:09 -0400

    did sieve

---
+++ makefile
@@ -0,0 +1,8 @@
+compile:
+	gcc siv.c -lm
+
+run:
+	./a.out
+
+clean:
+	@rm -rf ./a.out
--- siv.c
+++ siv.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <math.h>
 
-double find_nth_prime(double n) {
-    double arr[1.15 * n * log(n)];
-    double i, temp, count;
-    arr[0] = 1;
-    arr[1] = 1;
-    for (i = 0; i < n; i++) {
-        if (! arr[i]) {
-            count += 1;
-            for (temp = i; temp < n; temp += arr[i]) {
-                arr[temp] = 1;
+int find_nth_prime(int n) {
+    char * arr = calloc(sizeof(char), ceil(1.15 * n * log(n)));
+    int num = ceil(1.15 * n * log(n));
+    int i, ret = 2;
+    // printf("This is the size: %d\n", num);
+    int count = 0;
+
+    // printf("0: %d, 1: %d\n", * (arr + 0), * (arr + 1));
+    for (i = 2; i < num; i++) {
+        // if current index is not 1 (true)
+        // printf("current val: %d\ncurrent index: %d\n\n", * (arr + i), i);
+        if (! * (arr + i)) {
+            // printf("bool worked\n");
+            for (ret = 2; ret * i < num; ret++) { // make all multiples 1
+                if (! * (arr + (ret * i))) {
+                    int temp = ret * i;
+                    * (arr + temp) = 1;
+                    // printf("val at index %d: %d\n", 4, * (arr + 4));
+                }
             }
+        }
+    } for (i = 2; count < n; i++) {
+        if (! * (arr + i)) {
+            count += 1;
+            ret = i;
         }
     }
 
+    // free(arr);
+    return ret;
+}
+
+int main() {
+    printf("Searching for: 1,000,000th prime: %d\n", find_nth_prime(1000000));
+    return 0;
 }
