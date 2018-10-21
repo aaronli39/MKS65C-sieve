33dd191c624ec618738cb35c46c828762cce33b8
Aaron Li <aaronli39@gmail.com>
2018-10-21 09:48:28 -0400

    finished

--- sieve.c
+++ sieve.c
@@ -37,7 +37,7 @@ int sieve0(int n) {
 
 int sieve(int n) {
     char * arr = calloc(sizeof(char), ceil(1.15 * n * log(n)));
-    int num = ceil(1.15 * n * log(n)) / 2;
+    int num = (ceil(1.15 * n * log(n)) / 2) + 3;
     int i, ret = 3;
     // printf("This is the size: %d\n", num);
     int count = 1;
@@ -47,10 +47,10 @@ int sieve(int n) {
         // if current index is not 1 (true)
         // printf("current val: %d\ncurrent index: %d\n\n", * (arr + i), i);
         if (! * (arr + i)) {
-            printf("%s\n", arr + i);
+            // printf("%s\n", arr + i);
             // printf("bool worked\n");
             int current_val = 3 + (2 * (i - 3));
-            for (ret = 3; ret * current_val < num; ret += 2) { // make all non-even multipltes 1
+            for (ret = 3; ret * current_val <= 3 + (2 * (num - 3)); ret += 2) { // make all non-even multipltes 1
                 // if (* (arr + (ret * i)) % 2 != 0) {
                 //     printf("NOt true  \n");
                 // }
@@ -58,12 +58,13 @@ int sieve(int n) {
                 int temp = (i + ((ret * (3 + (2 * (i - 3)))) - (3 + (2 * (i - 3)))) / 2);
                 if (! * (arr + temp)) {
                     * (arr + temp) = 1;
-                    if (i < 5) printf("val of %d: %d\n", ret * (3 + (2 * (i - 3))), * (arr + temp));
+                    // if (i < 5) printf("val of %d: %d\n", ret * (3 + (2 * (i - 3))), * (arr + temp));
                     // printf("val at index %d: %d\n", 4, * (arr + 4));
                 }
             }
         }
     } for (i = 3; count < n; i++) {
+        // printf("%d: %d\n", i, * (arr + i));
         if (! * (arr + i)) {
             count++;
             ret = 3 + (2 * (i - 3));
